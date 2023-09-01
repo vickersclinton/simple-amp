a513fe52cfc53e5f29aff861d5443c4a3faa90da
Clinton Vickers <vickers_clinton@yahoo.com>
2023-09-01 14:50:11 -0500

    complete app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "simpleRestApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "simpleAmpLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify'
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const myAPI = "simpleRestApi"
+const path = '/item'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [items, setItems] = useState([])
+
+  //Function to fetch from our backend and update customers array
+  function getItem(e) {
+    let itemId = e.input
+    API.get(myAPI, path + "/" + itemId)
+       .then(response => {
+         console.log(response)
+         let newItems = [...items]
+         newItems.push(response)
+         setItems(newItems)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="item id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getItem({input})}>Get Item From Backend</button>
+
+      <h2 style={{visibility: items.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       items.map((thisItem, index) => {
+         return (
+        <div key={thisItem.itemId}>
+          <span><b>ItemId:</b> {thisItem.itemId} - <b>CustomerName</b>: {thisItem.itemName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
