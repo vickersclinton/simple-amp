90762d0a231466f4dd270a041d65d2c80d57d139
Clinton Vickers <vickers_clinton@yahoo.com>
2023-09-01 14:58:00 -0500

    changed import for aws-exports in index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Amplify} from "aws-amplify";
-import awsExports from './aws-exports';
+// import awsExports from './aws-exports';
+import awsmobile from './aws-exports';
 
-Amplify.configure(awsExports);
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
